77dff252780e5f529ddf4182cf3f5b7382906819
tuncayuzun <gemisibatmayanadam@gmail.com>
2019-04-16 20:41:36 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./layout/Header";
 import AddBook from "./components/AddBook";
+import Footer from "./layout/Footer";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header name="LIB SYS"/>
-        <br/>
-        <div className="container">
-            <AddBook/>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header name="LIB SYS"/>
+                <br/>
+                <div className="container">
+                    <AddBook/>
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+
+const Footer=()=> {
+
+    return (
+                <footer>&copy; LibSYS {(new Date()).getFullYear()}</footer>
+
+
+    );
+}
+
+
+export default Footer;
